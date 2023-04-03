acf5688a6128bbe07d7d66c78d568e41076d7ea5
abdumomin <shamshinovabdumomin2003@gmail.com>
2023-04-03 23:02:27 +0500

    prosta builder.default

--- src/main/java/com/company/hasjob/repository/AdsRepository.java
+++ src/main/java/com/company/hasjob/repository/AdsRepository.java
@@ -3,7 +3,14 @@ package com.company.hasjob.repository;
 import com.company.hasjob.entity.Ads;
 import com.company.hasjob.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AdsRepository extends JpaRepository<Ads, Integer> {
+
+    @Query("select a from Ads a where a.isActive = true")
+    List<Ads> findAllByActiveTrue();
+
     Users getAllByActive();
 }
--- src/main/java/com/company/hasjob/repository/UsersRepository.java
+++ src/main/java/com/company/hasjob/repository/UsersRepository.java
@@ -1,6 +1,8 @@
 package com.company.hasjob.repository;
 
+import com.company.hasjob.entity.JobType;
 import com.company.hasjob.entity.Users;
+import org.apache.catalina.User;
 import com.google.common.base.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,11 +10,15 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface UsersRepository extends JpaRepository<Users,Integer> {
+    @Query("select u from Users u where u.phoneNumber = ?1")
     Users findByPhoneNumber(String phoneNumber);
     @Query("select u from Users u where u.status='ACTIVE' and u.authRole='USER' ")
     List<Users> findAllByStatus();
 
 
 
+
+    @Query("select u from Users u where u.status='ACTIVE' and u.authRole='USER' and u.job <> ?1")
+    List<Users> findAllByJob(JobType jobType);
 
 }
